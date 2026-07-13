5af7fec8fbddc6f5e30828c198c15e57a17e94b8
BeltBuckleBill <rubenventer853@gmail.com>
2026-07-13 10:54:25 +0200

    Create failure.php

---
+++ controllers/front/failure.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * failure.php
+ *
+ * Copyright (c) 2026 LinuxISP (Pty) Ltd
+ * You (being anyone who is not LinuxISP (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+ * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
+ *
+ * @author     Ruben Venter (ruben@linuxweb.co.za)
+ * @version    1.0.0
+ * @date       13/07/2026
+ *
+ * @link       https://github.com/Linuxweb/Payfast-ThirtyBees/
+ */
+class PayfastFailureModuleFrontController extends ModuleFrontController
+{
+    public function initContent()
+    {
+        parent::initContent();
+        $this->setTemplate('failure.tpl');
+    }
+}
